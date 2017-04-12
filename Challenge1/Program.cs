b0d5dbabb430d8e3fbbfc9297041d57c7d8dfccc
Charles Lambrecht <lambrechtca@gmail.com>
2017-04-12 13:47:12 -0400

    Initial commit

---
+++ Challenge1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long result = GetPower(8, 3);
+            Console.WriteLine(result);
+
+        }
+
+        public static long GetPower(int x, int y)
+        {
+            long n = x;
+
+            for (int i = 1; i <= y; i++)
+            {
+                n = n * x;
+            }
+
+            return n;
+        }
+
+    }
+}
